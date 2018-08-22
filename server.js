82cab68a8ceac273f69d59743e4d700eab868117
Phong Khoang <Phong Khoang@DESKTOP-BGFDQ7J>
2018-08-22 16:08:01 +1400

    The third commint, fix deprecated in fs.appendFile function (need to have callback function)

--- server.js
+++ server.js
@@ -35,7 +35,11 @@ hbs.registerHelper('funcHasPara', (message)=>{
 app.use((req, res, next)=>{
     var now = new Date().toString();
     var log = `${now}: ${req.method}, ${req.url}`;
-    fs.appendFile('server.log', log+'\n');
+    fs.appendFile('server.log', log+'\n', (err)=>{
+        if(err){
+            console.log("Unable to append to server.log");
+        }
+    });
     next();
 });
 // Midleware 2
